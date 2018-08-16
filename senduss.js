b3015f145e20f7cbc46c1affce0c2f7c61976bd8
babatonha <tonhaview@gmail.com>
2018-08-16 04:18:47 +0200

    javascript file

---
+++ senduss.js
@@ -0,0 +1,89 @@
+
+
+    
+$(function() {
+
+$('.heading').hide(200).show(122);
+
+
+
+ /*
+   var res = $('.selectRes');
+   var loaction = $('selectLocation');
+   
+
+
+
+   $(' #check').on(‘click’, function(){
+       
+  
+     if(res==="DTL 93 canterbury street" && location==="Gardens Mall"){
+        $('.displayFee').html('The price is R200');
+
+      } 
+
+      else  if(res==="Palm cour dummy 33 more street" && location==="Water Front Mall"){
+        displayFee.html('The price is R150');
+      }
+
+      else  if(res==="New Castle, 22 stevens road" && location==="Century City Mall"){
+        $('.displayFee').html('The price is R100');
+      } 
+
+      else{
+        $('.displayFee').html('The price is R50');
+      }*/
+
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+$(function() {
+	var width = 720;
+	var animationSpeed = 1000;
+	var pause = 3000;
+	var currentSlide = 1;
+
+	//cache the DOM
+	var $slider = $('#slider');
+	var slideContainer = $slider.find('.slides');
+	var $slides = $slideContainer.find('.slide');
+
+
+	setInterval(function() {
+		$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() 
+			{
+				curentSlide++;
+				if(currentSlide === $slide.length){
+					currentSlide = 1;
+					$slideContainer.css('margin-left', 0);
+				}
+
+		       });
+			}, pause);
+
+	$slider.on('mouseenter',pauseSlider).on('mouseleave', startSlider);
+
+	
+});*/
+
